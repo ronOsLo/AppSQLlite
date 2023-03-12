e9db0d030af663b6784cecaa62df23f706e280db
ronOsLo <92495345+ronOsLo@users.noreply.github.com>
2023-03-11 19:02:43 -0600

    Vercion 11.3.23

--- app/src/main/java/com/example/appsqllite/ActivityList.java
+++ app/src/main/java/com/example/appsqllite/ActivityList.java
@@ -2,13 +2,32 @@ package com.example.appsqllite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.appsqllite.configuracion.SQLliteConexion;
+import com.example.appsqllite.configuracion.transacciones;
+
+import java.util.ArrayList;
 
 public class ActivityList extends AppCompatActivity {
+    SQLliteConexion conexion;
+    ListView listaPersonas;
+    ArrayList<Personas> lista;
+    ArrayList<String> arregloPersonas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        conexion = new SQLliteConexion(this, transacciones.NemeDatabase,null,1);
+        listaPersonas = (ListView) findViewById(R.id.listaPersonas);
+        obtenerListaPersonas();
+    }
+
+    private void obtenerListaPersonas()
+    {
+
     }
 }
---
+++ app/src/main/java/com/example/appsqllite/Personas.java
@@ -0,0 +1,4 @@
+package com.example.appsqllite;
+
+public class Personas {
+}
